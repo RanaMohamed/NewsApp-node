d648e2a4df7449e7eac127472c34df2142280a72
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-18 13:55:21 +0200

    :tada: Initial Commit

---
+++ config.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const requiredEnvs = ['JWT_SECRET', 'DB_URI'];
+
+const missingEnvs = requiredEnvs.filter((envName) => !process.env[envName]);
+
+if (missingEnvs.length) {
+	throw new Error(`Missing required envs ${missingEnvs}`);
+}
+
+module.exports = {
+	saltRounds: process.env.SALT_ROUNDS || 7,
+	port: process.env.PORT || 3000,
+	jwtSecret: process.env.JWT_SECRET,
+	dbUri: process.env.DB_URI,
+};
---
+++ middlewares/authentication.js
@@ -0,0 +1,16 @@
+const User = require('../models/user');
+
+module.exports = async (req, res, next) => {
+	try {
+		let user;
+		if (req.headers.authorization) {
+			user = await User.getUserFromToken(req.headers.authorization);
+		}
+
+		if (!user) return res.status(401).json({ message: 'Unauthorized' });
+		req.user = user;
+		next();
+	} catch (err) {
+		next(err);
+	}
+};
